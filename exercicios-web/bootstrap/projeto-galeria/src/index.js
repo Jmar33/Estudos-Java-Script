71a4a523474b8ae25e6f93c6921b6ec3c4d3fb0f
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-01-18 08:21:11 -0300

    Incluindo galeria e cabeçalho da página

---
+++ exercicios-web/bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,9 @@
+//Arquivo SASS
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery' //arquivos sem extensão são interpretados como .js pelo webpack
+import 'bootstrap'
+
+// Meus arquivos JS
+import './js/core/includes'
