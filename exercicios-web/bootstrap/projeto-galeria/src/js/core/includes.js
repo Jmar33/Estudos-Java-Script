f0093e15ea3e39767748bc1c72a780ef330506c5
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-01-18 10:10:59 -0300

    Executando plugin a partir de uma chamada ajax

--- exercicios-web/bootstrap/projeto-galeria/src/js/core/includes.js
+++ exercicios-web/bootstrap/projeto-galeria/src/js/core/includes.js
@@ -1,5 +1,15 @@
 import $ from 'jquery'
 
+//Array que contém as funções que serão chamadas após um html ser carregado com sucesso
+const loadHtmlSuccessCallbacks = []
+
+//Inclui função no array de callbacks, caso a função ainda não esteja presente no array
+export function onLoadHtmlSuccess(callback) {
+    if(!loadHtmlSuccessCallbacks.includes(callback)){
+        loadHtmlSuccessCallbacks.push(callback)
+    }
+}
+
 function loadIncludes(parent){
     if(!parent) parent = 'body'
     $(parent).find('[wm-include]').each(function(i, e) {
@@ -9,7 +19,8 @@ function loadIncludes(parent){
             success(data){
                 $(e).html(data)
                 $(e).removeAttr('wm-include')
-
+                loadHtmlSuccessCallbacks.forEach(
+                    callback => callback(data))
                 loadIncludes(e)
             }
         })
