f512fea49519441bd2b2e81128e65b9b32b888d0
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-01-23 17:25:39 -0300

    Criando o primeiro componente funcional

---
+++ exercicios-web/react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//Um tipo de componente React é o componente funcional, que como mostrado
+// a seguir, se baseia em uma função
+
+export default () => <h1>Primeiro 2</h1>
+
+// export default function(){
+//     return <h1>Primeiro</h1>
+// }
--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //O módulo React deve ser importado sempre que houver códigos JSX's no nossos arquivos
 
+import Primeiro from './componentes/Primeiro'
+
 const elemento  = <h1>React 2</h1> //JSX -> JS extendido
 
 //ReactDOM é responsável por controlar a DOM
-ReactDOM.render(elemento, document.getElementById('root'))
+ReactDOM.render(<Primeiro/>, document.getElementById('root'))
